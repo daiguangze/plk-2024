def0b16fc28e14c72aaf7028e0c025c93a2e3f71
daiguangze <daishuaishuai.z@qq.com>
2024-03-13 21:02:11 +0800

    .

---
+++ src/util/floodfill/FloodFill.java
@@ -0,0 +1,19 @@
+package util.floodfill;
+
+import model.Berth;
+
+import java.util.List;
+
+/**
+ * 洪水算法
+ * 使用洪水算法进行染色 确定每个泊位所负责的范围
+ */
+public class FloodFill {
+    private char[][] maps;
+
+    private Object result;
+
+    private List<Berth> berths;
+
+
+}
