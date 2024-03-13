877af48cc7c17a535d78128b0ae337847f5cf702
daiguangze <daishuaishuai.z@qq.com>
2024-03-14 03:17:46 +0800

    feat: 完成洪水, 待测试

--- src/util/floodfill/FloodFill.java
+++ src/util/floodfill/FloodFill.java
@@ -11,23 +11,63 @@ import java.util.*;
 public class FloodFill {
 
     public static Map<MapNode,String> getPointMessage(char[][] maps , List<Berth> berths) {
+        Map<MapNode,String> ans = new HashMap<>();
         int[][] visited = new int[maps.length][maps.length];
-        Deque<MapNode>[] queues = new ArrayDeque[berths.size()];
+        ArrayDeque<MapNode>[] queues = new ArrayDeque[berths.size()];
         // 放入初始节点
 
         // 广搜
         while(isEnd(queues)){
             for(int i = 0 ; i < berths.size() ; i++){
+                ArrayDeque<MapNode> queue = queues[i];
                 // 取出全部节点
-                // 加入周边合法节点
+                while(!queue.isEmpty()){
+                    MapNode mapNode = queue.removeFirst();
+                    addFrontiers(maps,visited,mapNode,queue,i,ans);
+                }
+
             }
         }
-        return null;
+        return ans;
     }
 
-    private static boolean isEnd(Deque<MapNode>[] queues) {
-        for(Deque deque : queues){
-            if (deque.size() != 0) return false;
+    private static void addFrontiers(char[][] maps, int[][] visited, MapNode mapNode,ArrayDeque<MapNode> queue,int i,Map<MapNode,String> ans) {
+        int x =  mapNode.x;
+        int y = mapNode.y;
+        // 下
+        if (isValid(maps,visited, x+1, y)){
+            visited[x+1][y] = i;
+            queue.addFirst(new MapNode(x+1,y));
+        }
+        // 右
+        if (isValid(maps,visited, x, y+1)){
+            visited[x][y+1] = i;
+            queue.addFirst(new MapNode(x,y+1));
+        }
+        // 上
+        if (isValid(maps,visited, x-1, y)){
+            visited[x-1][y] = i;
+            queue.addFirst(new MapNode(x-1,y));
+        }
+        // 左
+        if (isValid(maps,visited, x, y-1)){
+            visited[x][y-1] = i;
+            queue.addFirst(new MapNode(x,y-1));
+        }
+
+
+    }
+
+    private static boolean isValid(char[][] maps ,int[][] visited , int x, int y){
+        if (x < 0 || x >= maps.length ) return false;
+        if (y < 0 || y >= maps.length ) return false;
+        if (visited[x][y] != 0) return false;
+        if (maps[x][y] != '.') return false;
+        return true;
+    }
+    private static boolean isEnd(ArrayDeque<MapNode>[] queues) {
+        for(ArrayDeque deque : queues){
+            if (!deque.isEmpty()) return false;
         }
         return true;
     }
@@ -39,6 +79,11 @@ public class FloodFill {
 class MapNode {
     public int x;
     public int y;
+
+    public MapNode(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 
     @Override
     public boolean equals(Object obj) {
