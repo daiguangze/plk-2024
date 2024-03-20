77d64700fe070ba07ddcaaa4dbdd67ae6f5724df
daiguangze <daishuaishuai.z@qq.com>
2024-03-20 17:30:12 +0800

    temp : ..

---
+++ src/enums/RobotState.java
@@ -0,0 +1,7 @@
+package enums;
+
+public enum RobotState {
+    BORING,
+    FINDING_GOOD,
+    GO_BERTH,
+}
--- src/model/Robot.java
+++ src/model/Robot.java
@@ -1,7 +1,10 @@
 package model;
 
+import com.sun.org.apache.bcel.internal.generic.BREAKPOINT;
 import enums.RobotActionCode;
+import enums.RobotState;
 import instruction.Instruction;
+import org.omg.CORBA.PUBLIC_MEMBER;
 import util.astar.Coord;
 
 import java.util.ArrayDeque;
@@ -35,6 +38,8 @@ public class Robot {
      */
     public volatile int state;
 
+    public volatile RobotState robotState;
+
     public int mbx;
 
     public int mby;
@@ -54,11 +59,12 @@ public class Robot {
     }
 
     /**
-     *  通过欧式距离找出,距离该机器人最近的节点
+     * 通过欧式距离找出,距离该机器人最近的节点
+     *
      * @param goods
      * @return
      */
-    private Good findGood(List<Good> goods){
+    private Good findGood(List<Good> goods) {
 
         Good res = null;
         double minDistance = Double.MAX_VALUE;
@@ -77,45 +83,105 @@ public class Robot {
         }
         return res;
     }
+
     public RobotActionCode getMoveDirection(Coord next) throws Exception {
-        if(this.y != next.y){
-            if (this.y > next.y){
+        if (this.y != next.y) {
+            if (this.y > next.y) {
                 //左移
                 return RobotActionCode.LEFT;
-            }else{
+            } else {
                 //右移
                 return RobotActionCode.RIGHT;
             }
-        }else if(this.x != next.x){
-            if (this.x > next.x){
+        } else if (this.x != next.x) {
+            if (this.x > next.x) {
                 // 上移
                 return RobotActionCode.UP;
-            }else {
+            } else {
                 // 下移
                 return RobotActionCode.DOWN;
             }
         }
-        throw new Exception(String.format(" robot:[%d] 获取移动方向失败!!!   机器人坐标(%d,%d) , 下一步坐标(%d,%d)", this.id,this.x,this.y,next.x,next.y));
+        throw new Exception(String.format(" robot:[%d] 获取移动方向失败!!!   机器人坐标(%d,%d) , 下一步坐标(%d,%d)", this.id, this.x, this.y, next.x, next.y));
     }
+
     public String getMoveInstruction(Coord next) throws Exception {
         // 通过计算 robot坐标与 next坐标 判断是上下左右移动
-        if(this.y != next.y){
-            if (this.y > next.y){
+        if (this.y != next.y) {
+            if (this.y > next.y) {
                 //左移
                 return Instruction.leftString(this.id);
-            }else{
+            } else {
                 //右移
                 return Instruction.rightString(this.id);
             }
-        }else if(this.x != next.x){
-            if (this.x > next.x){
+        } else if (this.x != next.x) {
+            if (this.x > next.x) {
                 // 上移
                 return Instruction.upString(this.id);
-            }else {
+            } else {
                 // 下移
                 return Instruction.downString(this.id);
             }
         }
-        throw new Exception(String.format(" robot:[%d] 移动方向计算错误!!!   机器人坐标(%d,%d) , 下一步坐标(%d,%d)", this.id,this.x,this.y,next.x,next.y));
+        throw new Exception(String.format(" robot:[%d] 移动方向计算错误!!!   机器人坐标(%d,%d) , 下一步坐标(%d,%d)", this.id, this.x, this.y, next.x, next.y));
+    }
+
+    public boolean move(Coord next, int[][] conflictMap) throws Exception {
+        if (doMove(next,conflictMap)) {
+            return true;
+        }else{
+            return handleConflict(getMoveDirection(next),conflictMap);
+        }
+    }
+
+    public boolean doMove(Coord next, int[][] conflictMap) throws Exception {
+        if (!isConflict(next, conflictMap)) {
+            // 解锁
+            conflictMap[this.x][this.y] = -1;
+            //移动
+            System.out.println(getMoveInstruction(next));
+            // 锁定
+            conflictMap[next.x][next.y] = this.id;
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 判断是否产生冲突
+     *
+     * @param coord       next坐标
+     * @param conflictMap 冲突地图
+     */
+    private boolean isConflict(Coord coord, int[][] conflictMap) {
+        return conflictMap[coord.x][coord.y] != -1;
+    }
+
+    /**
+     * 处理冲突
+     *
+     * @param action 发生冲突的指令行为
+     */
+    private boolean handleConflict(RobotActionCode action, int[][] conflictMap) throws Exception {
+        switch (this.robotState){
+            case BORING:
+                break;
+            case FINDING_GOOD:
+                break;
+            // 前往泊位状态时冲突 不需要维护指令队列
+            case GO_BERTH:
+                switch (action){
+                    case UP:
+                    case DOWN:
+                        doMove(new Coord(this.x,this.y+1),conflictMap);
+                        break;
+                    case LEFT:
+                    case RIGHT:
+                        break;
+                }
+                break;
+        }
+        return false;
     }
 }
