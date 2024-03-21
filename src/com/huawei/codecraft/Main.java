493cdef2558dc0666ff539c5dc516cb714d441ce
daiguangze <daishuaishuai.z@qq.com>
2024-03-21 22:38:59 +0800

    style : TestMain 转正

--- src/com/huawei/codecraft/Main.java
+++ src/com/huawei/codecraft/Main.java
@@ -1,19 +1,15 @@
 package com.huawei.codecraft;
 
-import operator.FinalOperator;
 import operator.TestOperator;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
 
-
-
     public static void main(String[] args) throws FileNotFoundException {
         Scanner in = new Scanner(System.in);
-//        Scanner in = new Scanner(new File(Main.class.getResource("/").toString().substring(6) + "\\log.txt"));
+//        Scanner in = new Scanner(new File("D:\\HUAWEI\\WindowsReleasev1.1\\log.txt"));
 //        OperatorImpl operator = new OperatorImpl(in);
 //        operator.init();
 //        operator.run();
@@ -21,12 +17,8 @@ public class Main {
 //            in.nextLine();
 //        }
 
-//        FinalOperator operator = new FinalOperator(in);
         TestOperator operator = new TestOperator(in);
         operator.run();
 
     }
-
-
-
 }
